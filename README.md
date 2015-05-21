23aa0e59ca36446481db664d1abcdbac3d67e160
unknown <zhangtao01@PC-06965.sh.the9.com>
2015-05-21 16:55:44 +0800

    重新上传的第二个版本
    Signed-off-by: unknown <zhangtao01@PC-06965.sh.the9.com>

--- README.md
+++ README.md
@@ -1,8 +1,31 @@
-# MSSIframe
-MSSIframe   MSSUI单独脱离出来的一个外壳构架
+# MSSIframe V2.0
+
+这个版本可以自适应手机
 
 这是个做好的后台管理框架的最外面一层iframe嵌套，喜欢的人可以下载去用，不会的可以联系我
 
 我不是什么大神，封装的能用，但不是最好，介意的别看.不介意的想用的自行下载.
 
+对iframe提供了3种方法
 
+
+    //这里就是调用顶级父窗口打开页面的方法
+    top.MSS_Iframe.openIframe({
+            title:"打开一个新窗口",
+            url:"...",
+            key:"Q123"//需要和外部所有的ID不一致，如果有重复，会帮你加个时间戳
+    });
+
+    
+    //这里是关闭对应key的
+    top.MSS_Iframe.closeIframe("Q123");
+
+
+    //这里是关闭对应key的
+    top.MSS_Iframe.sendMsg({
+            text: "我是发送的信息，我延迟关闭",
+            location: "bottom,right",//位置有4种 top,bottom 在前 left,right 在后
+            secs: 3//秒，延时消失时间
+    });
+    background: "#ff7e00",//自定义背景
+    color: "#ffffff",//自定义颜色
---
+++ json/menu.js
@@ -0,0 +1,187 @@
+var menuData = [
+    {
+        "id": 1,
+        "text": "一级菜单A",
+        "href": "",
+        "nodes": [
+            {
+                "id": 4,
+                "text": "二级菜单Aa",
+                "href": "",
+                "nodes": [
+                    {
+                        "id": 13,
+                        "text": "三级菜单Aa1",
+                        "href": "",
+                        "nodes": [
+                            {
+                                "id": 21,
+                                "text": "四级菜单Aa1A",
+                                "href": "",
+                                "nodes": [
+                                    {
+                                        "id": 23,
+                                        "text": "五级菜单Aa1A1",
+                                        "href": "app/Layout.html",
+                                        "nodes": []
+                                    },
+                                    {
+                                        "id": 24,
+                                        "text": "五级菜单Aa1A2",
+                                        "href": "app/Layout.html",
+                                        "nodes": []
+                                    }
+                                ]
+                            },
+                            {
+                                "id": 22,
+                                "text": "四级菜单Aa1B",
+                                "href": "app/Layout.html",
+                                "nodes": []
+                            }
+                        ]
+                    },
+                    {
+                        "id": 14,
+                        "text": "三级菜单Aa2",
+                        "href": "app/Layout.html",
+                        "nodes": []
+                    }
+                ]
+            },
+            {
+                "id": 5,
+                "text": "二级菜单Ab",
+                "href": "",
+                "nodes": [
+                    {
+                        "id": 15,
+                        "text": "三级菜单Ab1",
+                        "href": "",
+                        "nodes": [
+                            {
+                                "id": 25,
+                                "text": "四级菜单Ab1A",
+                                "href": "app/Layout.html",
+                                "nodes": []
+                            },
+                            {
+                                "id": 26,
+                                "text": "四级菜单Ab1A",
+                                "href": "app/Layout.html",
+                                "nodes": []
+                            }
+                        ]
+                    },
+                    {
+                        "id": 16,
+                        "text": "三级菜单Ab2",
+                        "href": "app/Layout.html",
+                        "nodes": []
+                    }
+                ]
+            },
+            {
+                "id": 6,
+                "text": "二级菜单Ac",
+                "href": "",
+                "nodes": []
+            }
+        ]
+    },
+    {
+        "id": 2,
+        "text": "一级菜单B",
+        "href": "",
+        "nodes": [
+            {
+                "id": 7,
+                "text": "二级菜单Ba",
+                "href": "",
+                "nodes": [
+                    {
+                        "id": 17,
+                        "text": "三级菜单Ba1",
+                        "href": "app/Layout.html",
+                        "nodes": []
+                    },
+                    {
+                        "id": 18,
+                        "text": "三级菜单Ba2",
+                        "href": "app/Layout.html",
+                        "nodes": []
+                    }
+                ]
+            },
+            {
+                "id": 8,
+                "text": "二级菜单Bb",
+                "href": "",
+                "nodes": [
+                    {
+                        "id": 19,
+                        "text": "二级菜单Bb1",
+                        "href": "app/Layout.html",
+                        "nodes": []
+                    },
+                    {
+                        "id": 20,
+                        "text": "二级菜单Bb2",
+                        "href": "app/Layout.html",
+                        "nodes": []
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "id": 3,
+        "text": "一级菜单C",
+        "href": "",
+        "nodes": [
+            {
+                "id": 10,
+                "text": "二级菜单Ca",
+                "href": "app/Layout.html",
+                "nodes": []
+            },
+            {
+                "id": 11,
+                "text": "二级菜单Cb",
+                "href": "app/Layout.html",
+                "nodes": []
+            },
+            {
+                "id": 12,
+                "text": "二级菜单Cc",
+                "href": "app/Layout.html",
+                "nodes": []
+            }
+        ]
+    },
+    {
+        "id": 4,
+        "text": "一级菜单D",
+        "href": "",
+        "nodes": [
+            {
+                "id": 108,
+                "text": "二级菜单Dad",
+                "href": "app/Layout.html",
+                "nodes": []
+            },
+            {
+                "id": 181,
+                "text": "二级菜单Dbd",
+                "href": "app/Layout.html",
+                "nodes": []
+            },
+            {
+                "id": 182,
+                "text": "二级菜单Dcd",
+                "href": "app/Layout.html",
+                "nodes": []
+            }
+        ]
+    }
+]
